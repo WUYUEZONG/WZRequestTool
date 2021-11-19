dfb2af49bf1122a1aae2964e3d5bcb1b770fc654
WUYUEZONG <w.yzong@outlook.com>
2021-11-19 10:03:40 +0800

    1

--- Package.swift
+++ Package.swift
@@ -20,7 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "WZRequestTool",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Alamofire", package: "Alamofire")
+            ]),
         .testTarget(
             name: "WZRequestToolTests",
             dependencies: ["WZRequestTool"]),
